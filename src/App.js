5d6267b72736bb2ed8623c4d6db2b1a2265e4ed7
Felipe <fpizarro.ux@gmail.com>
2022-07-11 00:02:09 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import JSONDATA from './MOCK_DATA.json';
+import {useState} from 'react';
 
 function App() {
+
+  const [searchWords, setSearchWords] = useState('')
   return (
     <div className="App">
-      <input type="text" placeholder="Search"/>
-      {JSONDATA.map((v,key)=> {
-        return <div>{v.first_name}</div>
+      <input type="text" placeholder="Search" onChange={(event) =>{ setSearchWords(event.target.value) }}/>
+      {JSONDATA.filter((val)=> {
+        if(searchWords == ""){
+          return
+        } else if(val.first_name.toLowerCase().includes(searchWords.toLowerCase())){
+          return val
+
+        }
+      }).map((v,key)=> {
+        return (
+        <div className="user" key={key}>
+          <p>{v.first_name}</p>
+          </div>
+        )
       }
       )}
     </div>
